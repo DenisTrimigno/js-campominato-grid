f596b9dce226f558a7392fbee52c0b9decc2aa46
MyUsernameDenisTrimignogit config --global user.email my@email.comdenis@trimigno.it <denis@trimigno.it>
2023-05-29 23:10:46 +0200

    fine esercizio base

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
+// funzione che crea la griglia di gioco
+function createNewGame(){
+    const grid = document.getElementById('grid');
+
+// svuota la griglia
+    grid.innerHTML= '';
+
+// funzione per creare le singole caselle di gioco
+    createCell(); 
+
+}
+
+// funzione che crea caselle della griglia
+function createCell(){
+        // generatore di caselle
+        for(let i = 0; i < 100; i++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            square.innerText = i + 1;
+    
+            square.addEventListener('click', function(){
+                this.classList.add('clicked');
+                console.log(this.innerText);
+            })
+    
+            grid.append(square);
+    
+        }
+}
+
 const button = document.getElementById('play')
 
 button.addEventListener('click', function(){
-    const grid = document.getElementById('grid');
-
-
-    grid.innerHTML= '';
-
-
-    // generatore di caselle
-    for(let i = 0; i < 100; i++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        grid.append(square)
-
-    }
+    // click del pulsante che crea la griglia di gioco
+    createNewGame();
 })
